c3cd68500778cbfaaaa10f5a9a5738efa014448b
jcw1031 <jcw001031@gmail.com>
2023-07-30 21:36:47 +0900

    ObjectMapper 커스텀 -> SnakeCase

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -1,11 +1,13 @@
 package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
-import org.delivery.db.account.AccountEntity;
+import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/account")
@@ -14,9 +16,12 @@ public class AccountApiController {
 
     private final AccountRepository accountRepository;
 
-    @GetMapping
-    public void save() {
-        AccountEntity account = AccountEntity.builder().build();
-        accountRepository.save(account);
+    @GetMapping("/me")
+    public AccountMeResponse me() {
+        return AccountMeResponse.builder()
+                .name("홍길동")
+                .email("example@gmail.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
     }
 }
